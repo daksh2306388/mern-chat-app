c87eb991eca0cd7e87e2cac6726d93c14dba3201
Daksh Patel <dakshdaksh543@gmail.com>
2026-01-12 19:19:05 +0530

    Simplify CORS config with direct origin string

--- server/src/app.js
+++ server/src/app.js
@@ -19,13 +19,7 @@ const allowedOrigins = [
 // CORS setup
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://mern-chat-app-gamma-jet.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
